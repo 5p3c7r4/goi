3c9f13655cf4a01a79bc39b72e6e76d8a6a879fe
5P3C7R4 <juanesvargas1@gmail.com>
2025-01-10 12:18:04 -0500

    v0.0.1-alpha

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/5p3c7r4/goi
+
+go 1.23.2
---
+++ goi/Decode.go
@@ -0,0 +1,65 @@
+package goi
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+func CustomDecode(src map[string]any, dst any) error {
+	dstVal := reflect.ValueOf(dst).Elem()
+	dstType := reflect.TypeOf(dst).Elem()
+
+	for i := 0; i < dstVal.NumField(); i++ {
+		field := dstVal.Field(i)
+		fieldType := dstType.Field(i)
+		tag := fieldType.Tag.Get("goi")
+
+		if value, exists := src[tag]; exists {
+			switch field.Kind() {
+			case reflect.Pointer:
+				if fieldType.Type.Elem() != reflect.TypeOf(value) {
+					panic(fmt.Sprintf("bad type, source type %s destination type %s", reflect.TypeOf(value).String(), fieldType.Type.Elem().String()))
+				}
+				castedValue := reflect.ValueOf(value).Convert(reflect.TypeOf(value))
+				ptr := reflect.New(reflect.TypeOf(value)).Elem()
+				ptr.Set(castedValue)
+				field.Set(reflect.ValueOf(ptr.Addr().Interface()))
+			case reflect.String:
+				if strVal, ok := value.(string); ok {
+					field.SetString(strVal)
+				} else {
+					panic(fmt.Sprintf("expected string for %s, got %T", tag, value))
+				}
+			case reflect.Int:
+				if intVal, ok := value.(int); ok {
+					field.SetInt(int64(intVal))
+				} else if strVal, ok := value.(string); ok {
+					parsedInt, err := strconv.Atoi(strVal)
+					if err != nil {
+						panic(fmt.Sprintf("failed to convert string '%s' to int", strVal))
+					}
+					field.SetInt(int64(parsedInt))
+				} else {
+					panic(fmt.Sprintf("expected int for %s, got %T", tag, value))
+				}
+			case reflect.Bool:
+				if boolVal, ok := value.(bool); ok {
+					field.SetBool(boolVal)
+				} else if strVal, ok := value.(string); ok {
+					parsedBool, err := strconv.ParseBool(strVal)
+					if err != nil {
+						panic(fmt.Sprintf("failed to convert string '%s' to bool", strVal))
+					}
+					field.SetBool(parsedBool)
+				} else {
+					panic(fmt.Sprintf("expected bool for %s, got %T", tag, value))
+				}
+			default:
+				panic(fmt.Sprintf("unsupported type %s for field %s", field.Kind(), fieldType.Name))
+			}
+		}
+	}
+
+	return nil
+}
